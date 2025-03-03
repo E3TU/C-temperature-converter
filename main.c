d9fb1d15a4e787c1b7c444a529630884aa3e628f
Eetu <99178022+E3TU@users.noreply.github.com>
2025-03-03 19:54:03 +0200

    Temperature converter logic

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int userchoice;
+    float celcius, kelvin, fahrenheit;
+    int i = 1;
+
+    while (1) {
+        printf("Temperature converter\n");
+        printf("1. Celsius\n");
+        printf("2. Fahrenheit\n");
+        printf("3. Kelvin\n");
+        printf("Choose the unit you want to convert: ");
+        scanf("%d", &userchoice);
+
+        switch (userchoice) {
+            case 1:
+                printf("Enter the temperature in celcius: ");
+                scanf("%f", &celcius);
+                fahrenheit = (celcius * 9/5) + 32;
+                kelvin = celcius + 273.15;
+                printf("\n");
+                printf("Temperature in Celcius: %.2f\n", celcius);
+                printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
+                printf("Temperature in Kelvin: %.2f\n", kelvin);
+                break;
+            case 2:
+                printf("Enter the temperature in fahrenheit: ");
+                break;
+            case 3:
+                printf("Enter the temperature in kelvin: ");
+                break;
+            default:
+                printf("Invalid option! Try again!\n");
+                break;
+        }
+        return 0;
+    }
+}
